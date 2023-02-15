d7bc2b7634a4790e7de7f7a7522d26e9bf370361
Sergio <sergiot.fontal@gmail.com>
2023-02-15 07:55:58 +0100

    Add App component Styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,15 @@
 import CreateForm from "../CreateForm/CreateForm";
 import Header from "../Header/Header";
+import AppStyled from "./AppStyled";
 
 const App = (): JSX.Element => {
   return (
-    <>
+    <AppStyled>
       <Header />
       <main>
         <CreateForm />
       </main>
-    </>
+    </AppStyled>
   );
 };
 
---
+++ src/components/App/AppStyled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  main {
+    display: flex;
+    justify-content: center;
+  }
+`;
+export default AppStyled;
